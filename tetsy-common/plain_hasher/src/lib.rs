759a58f4407481f552762eb8a622db2539643635
marlonhanks <marlon.hanks@kedia.co>
2021-02-25 09:57:21 -0500

    tetsy-util-mem@0.5.2 publish

---
+++ tetsy-common/plain_hasher/src/lib.rs
@@ -0,0 +1,59 @@
+// Copyright 2020 Parity Technologies
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use core::hash::Hasher;
+
+use crunchy::unroll;
+
+/// Hasher that just takes 8 bytes of the provided value.
+/// May only be used for keys which are 32 bytes.
+#[derive(Default)]
+pub struct PlainHasher {
+	prefix: u64,
+}
+
+impl Hasher for PlainHasher {
+	#[inline]
+	fn finish(&self) -> u64 {
+		self.prefix
+	}
+
+	#[inline]
+	#[allow(unused_assignments)]
+	fn write(&mut self, bytes: &[u8]) {
+		debug_assert!(bytes.len() == 32);
+		let mut bytes_ptr = bytes.as_ptr();
+		let mut prefix_ptr = &mut self.prefix as *mut u64 as *mut u8;
+
+		unroll! {
+			for _i in 0..8 {
+				unsafe {
+					*prefix_ptr ^= (*bytes_ptr ^ *bytes_ptr.offset(8)) ^ (*bytes_ptr.offset(16) ^ *bytes_ptr.offset(24));
+					bytes_ptr = bytes_ptr.offset(1);
+					prefix_ptr = prefix_ptr.offset(1);
+				}
+			}
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn it_works() {
+		let mut bytes = [32u8; 32];
+		bytes[0] = 15;
+		let mut hasher = PlainHasher::default();
+		hasher.write(&bytes);
+		assert_eq!(hasher.prefix, 47);
+	}
+}
---
+++ tetsy-common/tetsy-util-mem/README.md
@@ -0,0 +1,17 @@
+# tetsy-util-mem
+
+Collection of memory related utilities.
+
+## Features
+
+- estimate-heapsize : Do not use allocator, but `size_of` or `size_of_val`.
+
+Others features define global allocator, see `src/alloc.rs`.
+
+## Dependency
+
+This crate groups common dependency, a patched copy of unpublished [`malloc_size_of`](https://github.com/servo/servo/tree/master/components/malloc_size_of) from servo project is copied and partially reexported.
+
+`Malloc_size_of` code is used internally as a module with a few modification to be able to implement type locally.
+
+For existing code using deprecated `HeapsizeOf` crate, calls to `heapsize_of_children` should be replace by calls to `size_of`.
